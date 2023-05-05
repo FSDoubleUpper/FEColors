20c4f3cf8ec04b40278e4c5a06f8dccdafce7ac3
Yao Lee <fsdoubleupper@gmail.com>
2023-05-05 13:40:34 -0700

    working customText edit

--- FEColors/src/components/Main.jsx
+++ FEColors/src/components/Main.jsx
@@ -2,9 +2,9 @@ import style from "../css/main.css";
 import React, { useState } from "react";
 
 export default function Main() {
-    const [colorText, setColorText] = useState("rgb(0,0,0)");
+    const [colorText, setColorText] = useState("rgb(255,255,255)");
     const [colorBackground, setColorBackground] = useState("rgb(0,0,0)");
-    const [customTxt, setCustomTxt] = useState("Custom Text");
+    const [customText, setCustomText] = useState("Custom Text");
 
     const generateRandomColorText = () => {
         const r = Math.floor(Math.random() * 255);
@@ -27,51 +27,42 @@ export default function Main() {
         document.getElementsByClassName("color-background");
     };
 
-    function handleClick(event) {
-        setCustomTxt(event.target.innerText);
-        event.target.contentEditable = false;
-    }
+    const handleCustomText = (e) => setCustomText((p) => (p = e.target.value));
 
     return (
-        <>
-            <div className="wrapper">
-                <h1 className="title">FEColors</h1>
-                <p
-                    className="color-txt"
-                    style={{
-                        color: colorText,
-                        backgroundColor: colorBackground,
-                    }}
-                    onClick={handleClick}
-                >
-                    {customTxt}
-                </p>
-                <div className="color-info-container">
-                    <span className="color-info">text: </span>
-                    {colorText}
-                    <br />
-                    <span className="color-info">background: </span>
-                    {colorBackground}
-                </div>
-                <button
-                    className="random-btn"
-                    onClick={generateRandomColorText}
-                >
-                    Randomize TEXT color!
-                </button>
-                <button
-                    className="random-btn"
-                    onClick={generateRandomColorBoth}
-                >
-                    Randomize BOTH colors!
-                </button>
-                <button
-                    className="random-btn"
-                    onClick={generateRandomColorBackground}
-                >
-                    Randomize BACKGROUND color!
-                </button>
+        <div className="wrapper">
+            <h1 className="title">FEColors</h1>
+            <p
+                className="color-txt"
+                style={{
+                    color: colorText,
+                    backgroundColor: colorBackground,
+                }}
+                placeholder="Custom Text"
+                onClick={handleCustomText}
+                contentEditable={true}
+            >
+                {customText}
+            </p>
+            <div className="color-info-container">
+                <span className="color-info">text: </span>
+                {colorText}
+                <br />
+                <span className="color-info">background: </span>
+                {colorBackground}
             </div>
-        </>
+            <button className="random-btn" onClick={generateRandomColorText}>
+                Randomize TEXT color!
+            </button>
+            <button className="random-btn" onClick={generateRandomColorBoth}>
+                Randomize BOTH colors!
+            </button>
+            <button
+                className="random-btn"
+                onClick={generateRandomColorBackground}
+            >
+                Randomize BACKGROUND color!
+            </button>
+        </div>
     );
 }
